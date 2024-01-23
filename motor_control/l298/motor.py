c3841cb580cbbc209741c6b876fc7b42ef9bcb76
Christian Hellum Bye <chbye@berkeley.edu>
2024-01-23 19:01:41 +0000

    move files to new folder

--- motor_control/l298/motor.py
+++ motor_control/l298/motor.py
@@ -1,6 +1,6 @@
 import time
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 
 # import IPython
 from RPi import GPIO
@@ -12,11 +12,13 @@ FEED1 = 17
 FEED2 = 27
 
 
-def plot_live(feed1, feed2, npoints=1000):
+def plot_live(feed1, feed2, npoints=100):
     feeds = [feed1, feed2]
     plt.ion()
     fig = plt.figure()
     ax = fig.add_axes([0, 0, 1, 1])
+    ax.set_ylim(-.1, 1.1)
+    ax.grid()
     y = np.zeros((2, npoints))
     lines = []
     for i in range(2):
@@ -29,15 +31,20 @@ def plot_live(feed1, feed2, npoints=1000):
             lines[i].set_ydata(y[i])
         fig.canvas.draw()
         fig.canvas.flush_events()
-        time.sleep(0.1)
+        time.sleep(0.005)
 
 
 if __name__ == "__main__":
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup([IN1, IN2], GPIO.OUT)
-    GPIO.setup([FEED1, FEED2], GPIO.IN)
+    #GPIO.setup([FEED1, FEED2], GPIO.IN)
     GPIO.output(23, 0)
-    GPIO.output(24, 1)
-    plot_live(FEED1, FEED2)
+    GPIO.output(24, 0)
+#    try:
+ #       plot_live(FEED1, FEED2)
+ #   except KeyboardInterrupt:
+ #       print("Cancelling.")
+ #   finally:
+#        GPIO.cleanup()
 #    IPython.embed()
